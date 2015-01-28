860313b3c3472cab494db0feebb8ef7abf6d53c3
Philippe Coval <philippe.coval@open.eurogiciel.org>
2015-01-28 17:53:55 +0100

    Initial import on version 0.1.0
    Code is based on iotivity sample code from upstream :
    
      URL: https://gerrit.iotivity.org/gerrit/gitweb?p=iotivity.git;a=commit;h=7cd6e2214c0eb9a70ac8cb94b38f9dc682a6691e
      Author: Kishen Maloor <kishen.maloor@intel.com>
    
    It has been shrinked to do the minimal operation :
    like just blinking a led
    and refactored a bit.
    
    It was tested on Tizen:Common
    running on 2 minnowmax
    
    Change-Id: I695dc0ed65f8b32efdd0e65f9ff00c43e07ad0c3
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

---
+++ src/server.h
@@ -0,0 +1,53 @@
+// -*-c++-*-
+//******************************************************************
+//
+// Copyright 2014 Intel Corporation.
+// Copyright 2015 Eurogiciel <philippe.coval@eurogiciel.fr>
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef SERVER_H_
+#define SERVER_H_
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+#include <OCPlatform.h>
+#include <OCApi.h>
+
+
+class IoTServer
+{
+  std::shared_ptr<OC::PlatformConfig> m_platformConfig;
+  OC::OCRepresentation m_ledRepresentation;
+  OCResourceHandle m_ledResource;
+
+  void initializePlatform();
+  void setupResources();
+  void createResource(std::string, std::string, OC::EntityHandler, OCResourceHandle&);
+
+  OC::OCRepresentation getLEDRepresentation();
+  void putLEDRepresentation();
+  OCEntityHandlerResult LEDEntityHandler(std::shared_ptr<OC::OCResourceRequest>);
+
+public:
+  IoTServer(int pin=0, std::string key="switch");
+  virtual ~IoTServer();
+};
+
+#endif /* SERVER_H_ */
