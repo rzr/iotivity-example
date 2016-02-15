721cd720f7e17729d90aae46854acc6a5bdf8e22
Philippe Coval <philippe.coval@osg.samsung.com>
2016-02-15 12:32:39 +0100

    WIP: add: debian/iotivity-example.default.ex (debian)
    Change-Id: Ic73fab1e215f14318bbd9f6f184446402762ffd0
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ debian/iotivity-example.default.ex
@@ -0,0 +1,10 @@
+# Defaults for iotivity-example initscript
+# sourced by /etc/init.d/iotivity-example
+# installed at /etc/default/iotivity-example by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
