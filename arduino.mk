13f748559a5cca16dd874c91d6eb6f1c7d4ac3aa
Philippe Coval <philippe.coval@osg.samsung.com>
2016-09-23 18:17:42 +0200

    WIP: add: arduino.mk (sandbox/pcoval/csdk)
    Change-Id: I6a907bd8b98dc541403c65e02b0c59a078ddf2d7
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ arduino.mk
@@ -0,0 +1,132 @@
+default: all
+
+all: ${TARGET_ELF}
+
+config_pkgconfig=0
+include iotivity.mk
+
+ARDUINO_DIR = /usr/share/arduino
+AVR_TOOLS_DIR = ${ARDUINO_DIR}/hardware/tools/avr/
+BOARD_TAG = mega2560
+MONITOR_PORT = /dev/ttyACM*
+ARCHITECTURE=avr
+
+MONITOR_BAUDRATE = 115200
+AVRDUDE = ${ARDUINO_DIR}/hardware/tools/avrdude
+AVRDUDE_CONF = ${ARDUINO_DIR}/hardware/tools/avrdude.conf
+
+# hack to use arduino c++ libraries
+#LOCAL_C_SRCS = src/server.c
+LOCAL_CPP_SRCS = src/servercxx.cpp
+
+platform=arduino
+
+#{ configuration
+eth_enabled?=1
+CPPFLAGS += -DCONFIG_GPIO_PIN=13
+CPPFLAGS += -DCONFIG_MAC_1=0x90
+CPPFLAGS += -DCONFIG_MAC_2=0xA2
+CPPFLAGS += -DCONFIG_MAC_3=0xDA
+CPPFLAGS += -DCONFIG_MAC_4=0x0F
+CPPFLAGS += -DCONFIG_MAC_5=0x97
+CPPFLAGS += -DCONFIG_MAC_6=0xB3
+
+CPPFLAGS += -DARDUINOSERIAL=1
+ARDUINO_LIBS += Wire 
+ARDUINO_LIBS += SoftwareSerial
+#ARDUINO_LIBS += HardwareSerial
+#}
+arch=${ARCHITECTURE}
+arch=avr
+iotivity_out=${iotivity_dir}/out/${platform}/${arch}/${mode}
+
+#{ iotivity
+CXXFLAGS += -std=gnu++11
+
+iotivity_libs?=\
+${iotivity_out}/resource/csdk/liboctbstack.a \
+${iotivity_out}/resource/csdk/routing/libroutingmanager.a \
+${iotivity_out}/resource/csdk/security/libocsrm.a \
+${iotivity_out}/resource/csdk/logger/liblogger.a \
+${iotivity_out}/resource/c_common/libc_common.a \
+${iotivity_out}/resource/csdk/connectivity/src/libconnectivity_abstraction.a \
+#eol
+#iotivity_libs+=${iotivity_out}/libcoap.a
+
+
+iotivity_libs_eth?=${iotivity_out}/libEthernet.a
+#eol
+
+ifeq (${eth_enabled}, 1)
+ARDUINO_LIBS += Ethernet SPI
+CPPFLAGS += -DARDUINOETH=1
+iotivity_libs += ${iotivity_libs_eth}
+endif
+
+
+# for iotivity-1.1.0
+ifeq (${iotivity_version}, TODO_1.1.0)
+iotivity_libs+=\
+ ${iotivity_out}/libRBL_nRF8001.a \
+ ${iotivity_out}/libBLE.a \
+ #eol
+endif
+
+iotivity_libs+=\
+ ${iotivity_out}/Time/libTime.a \
+ ${iotivity_out}/libSPI.a \
+ #eol
+
+#}
+USER_LIB_PATH+= ${iotivity_includedirs}
+
+
+#OTHER_OBJS += ${iotivity_libs}
+
+#TODO:
+#core_lib=build-${BOARD_TAG}/libcore.a
+core_lib=${iotivity_out}/arduino/libmega_at${BOARD_TAG}_core.a
+#CORE_LIB=${core_lib}
+#LIBS+=${core_lib}
+iotivity_libs += ${core_lib}
+LIBS+=${iotivity_libs}
+
+${iotivity_libs}: ${iotivity_out}
+	date
+
+src/servercxx.cpp: src/server.c
+	@echo '#include "server.c"' >$@
+
+
+
+${iotivity_out}: ${iotivity_dir} ${iotivity_dir}/extlibs/tinycbor/tinycbor scons
+	sync
+#	cd $< && ./auto_build.sh ${platform}
+
+
+scons_flags += TARGET_OS=arduino UPLOAD=false BOARD=mega \
+ TARGET_ARCH=avr SHIELD=ETH \
+ #eol
+
+scons: ${iotivity_dir} tinycbor
+	cd $< && scons resource ${scons_flags}
+ #eol
+
+arduino_mk_url?=https://github.com/sudar/Arduino-Makefile
+${ARDUINO_DIR}/Arduino.mk: 
+	@echo "install: ${arduino_mk_url} "
+	ls /etc/debian_version && sudo apt-get install arduino-mk
+
+#{
+# http://stackoverflow.com/questions/8188849/avr-linker-error-relocation-truncated-to-fit/38755757#38755757
+include ${ARDUINO_DIR}/Arduino.mk
+
+LDFLAGS += -lc -lm
+
+$(TARGET_ELF): $(LOCAL_OBJS) $(CORE_LIB) $(OTHER_OBJS)
+	$(CC) $(LDFLAGS) -o $@ $(LOCAL_OBJS) $(CORE_LIB) $(OTHER_OBJS)
+#}
+
+${TARGET_ELF}: ${LOCAL_OBJS} ${LIBS}
+	${CC} ${LDFLAGS} -o $@ ${^}
+	ls -l ${@}
