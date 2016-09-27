d495d4d586299271b86980ea78d1fd4b8532d8cd
Philippe Coval <philippe.coval@osg.samsung.com>
2016-09-27 19:19:37 +0200

    WIP: mod: src/client.c (sandbox/pcoval/csdk)
    Change-Id: I879190a88b29f67925c0c5c4f9f41d051ed90c36
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/client.c
+++ src/client.c
@@ -23,7 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <iotivity_config.h>
+//#include <iotivity_config.h> //1.2.0+
 #include <octypes.h>
 #include <ocstack.h>
 #include <ocpayload.h>
