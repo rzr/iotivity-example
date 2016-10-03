3d57ba5b4eeeeb71379594e892d714d7d2e84b6e
Philippe Coval <philippe.coval@osg.samsung.com>
2016-10-03 21:29:59 +0200

    WIP: mod: src/client.h (sandbox/pcoval/switch)
    Change-Id: I8b876d6229dd437faa5ae311b928fcc5bac41202
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/client.h
+++ src/client.h
@@ -34,41 +34,41 @@
 
 class Resource
 {
-        std::shared_ptr<OC::OCResource> m_resourceHandle;
-        OC::OCRepresentation m_Representation;
-        OC::GetCallback m_GETCallback;
-        OC::PutCallback m_PUTCallback;
-        void onGet(const OC::HeaderOptions &, const OC::OCRepresentation &, int);
-        void onPut(const OC::HeaderOptions &, const OC::OCRepresentation &, int);
-    public:
-        void get();
-        void put(int);
-        Resource(std::shared_ptr<OC::OCResource> resource);
-        virtual ~Resource();
+    std::shared_ptr<OC::OCResource> m_resourceHandle;
+    OC::OCRepresentation m_Representation;
+    OC::GetCallback m_GETCallback;
+    OC::PutCallback m_PUTCallback;
+    void onGet(const OC::HeaderOptions &, const OC::OCRepresentation &, int);
+    void onPut(const OC::HeaderOptions &, const OC::OCRepresentation &, int);
+public:
+    void get();
+    void put(bool);
+    Resource(std::shared_ptr<OC::OCResource> resource);
+    virtual ~Resource();
 };
 
 
 class IoTClient
 {
-        std::shared_ptr<Resource> m_platformResource;
-        std::shared_ptr<OC::PlatformConfig> m_platformConfig;
-        OC::FindCallback m_FindCallback;
-        void init();
-        void onFind(std::shared_ptr<OC::OCResource>);
-    public:
-        void print(std::shared_ptr<OC::OCResource> resource);
+    std::shared_ptr<Resource> m_platformResource;
+    std::shared_ptr<OC::PlatformConfig> m_platformConfig;
+    OC::FindCallback m_FindCallback;
+    void init();
+    void onFind(std::shared_ptr<OC::OCResource>);
+public:
+    void print(std::shared_ptr<OC::OCResource> resource);
 
-        static  int main(int argc, char *argv[]);
-    public:
-        std::shared_ptr<Resource> getPlatformResource();
-        void start();
-        IoTClient();
-        virtual ~IoTClient();
+    static  int main(int argc, char *argv[]);
+public:
+    std::shared_ptr<Resource> getPlatformResource();
+    void start();
+    IoTClient();
+    virtual ~IoTClient();
 
-        static void DisplayMenu();
+    static void DisplayMenu();
 
-        static IoTClient *getInstance();
-        static IoTClient *mInstance;
+    static IoTClient *getInstance();
+    static IoTClient *mInstance;
 };
 
 #endif /* CLIENT_H_ */
