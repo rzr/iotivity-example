993cc823dc979472fd36d7a2633edea59c328ca0
Philippe Coval <philippe.coval@osg.samsung.com>
2016-10-03 21:29:59 +0200

    WIP: add: src/common.h (sandbox/pcoval/switch)
    Change-Id: Ib789f83e500a29488eff5a1c662200dd6db98426
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ src/common.h
@@ -0,0 +1,79 @@
+// -*-c++-*-
+//******************************************************************
+//
+// Copyright 2014 Intel Corporation.
+// Copyright 2015 Eurogiciel <philippe.coval@eurogiciel.fr>
+// Copyright 2016 Samsung Electronics France SAS All Rights Reserved.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <string>
+
+#if !defined(PACKAGE)
+#define PACKAGE "Example"
+#endif
+
+
+
+/** Pseudo singleton class to store common configuration variables **/
+class Common
+{
+public:
+    /** public interface (used both sides) **/
+    static std::string  m_interface;
+    /** type of resource (used both sides) **/
+    static std::string  m_type;
+    /** url's path (used both sides) **/
+    static std::string  m_endpoint;
+    /** key (used both sides) **/
+    static std::string  m_propname;
+    /** network interface**/
+    static std::string m_link;
+    /** polling period**/
+    static const int m_period = 5;
+};
+
+#define STR_(x) #x
+#define STR(x) STR_(x)
+
+#include <iostream>
+
+class Logger
+{
+    const char* mMessage;
+public:
+
+    Logger(const char * message)
+        {
+            mMessage = message;
+            std::cerr<<"log: { " << mMessage<<std::endl;
+        }
+    ~Logger()
+        {
+            std::cerr<<"log: } " << mMessage<<std::endl;
+        }
+};
+
+#define LOG()                                   \
+    Logger loggerObject(__PRETTY_FUNCTION__)
+
+
+//TODO: << " @"<< __FILE__<<":"<<__LINE__<<":"<<
+#endif /* COMMON_H_ */
