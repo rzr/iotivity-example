230376e5a2376e6745e55c1a26076834de81add5
Philippe Coval <philippe.coval@osg.samsung.com>
2016-10-04 23:16:15 +0200

    WIP:
    Change-Id: I565fcbc97bb6370158686d200f2ff1ff37c09ce2
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ src/main.h
@@ -0,0 +1,19 @@
+#ifndef __iotivityexample_H__
+#define __iotivityexample_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system/system_settings.h>
+#include <efl-extension/efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "iotivityexample"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.iotivityexample"
+#endif
+
+#endif /* __iotivityexample_H__ */
---
+++ tizen.mk
@@ -0,0 +1,56 @@
+#! /usr/bin/make -f
+
+default:all
+
+package?=iotivityexample
+
+iotivity_logo_url?=http://research.cisco.com/img/devnet-page/tiles/iotivity.jpg
+
+all?= tmp/512x512.png \
+ shared/res/${package}.png
+
+all: ${all}
+
+icon.png: docs/logo.png Makefile
+	convert -resize 117x117! $< $@
+
+docs/screenshot.png: docs/logo.png
+	echo cp $< $@
+
+tmp/screenshot.jpg: docs/screenshot.png
+	convert $< $@
+
+tmp/117x177/%: %
+	mkdir -p ${@D}
+	convert -resize '117x117!' $< $@
+
+
+tmp/512x512.png: docs/logo.png
+	mkdir -p ${@D}
+	convert -resize '512x512!' $< $@
+
+tmp/512x512.jpg: docs/logo.png
+	mkdir -p ${@D}
+	convert -resize '512x512!' $< $@
+
+distclean: clean
+	rm -f *.wgt
+	rm -rf Debug Release
+	rm -rf .package-stamp .settings .sign
+	rm -rf lib usr tmp
+
+clean:
+	rm -f *~
+
+docs/logo.jpg:
+	mkdir -p $@{D}
+	wget -O $@.tmp ${iotivity_logo_url}
+	convert $@.tmp $@
+
+
+docs/logo.png: docs/logo.svg
+	convert $< $@
+
+shared/res/${package}.png: tmp/117x177/docs/logo.png
+	cp $< $@
+
