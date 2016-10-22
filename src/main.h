549c95e0a6d7b0d4516e4c9816b7921097ba1ca3
Philippe Coval <philippe.coval@osg.samsung.com>
2016-10-22 10:21:47 +0200

    WIP: mod: src/main.h (sandbox/pcoval/on/sandbox/pcoval/geolocation/wip)
    Change-Id: Ia9937401325cea8dad39ff88f086c4429f1e1ade
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/main.h
+++ src/main.h
@@ -1,40 +1,23 @@
-// -*-c++-*-
-//******************************************************************
-//
-// Copyright 2016 Samsung Electronics France SAS All Rights Reserved.
-//
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-#ifndef __iotivityexample_H__
-#define __iotivityexample_H__
+#ifndef __LBS_MAPS_H__
+#define __LBS_MAPS_H__
 
 #include <app.h>
 #include <Elementary.h>
-#include <system/system_settings.h>
-#include <efl-extension/efl_extension.h>
+#include <system_settings.h>
+#include <efl_extension.h>
 #include <dlog.h>
 
-#ifdef  LOG_TAG
-#undef  LOG_TAG
-#endif
-#define LOG_TAG "iotivityexample"
+#include <tizen.h>
+#include <locations.h>
 
-#if !defined(PACKAGE)
-#define PACKAGE "org.example.iotivityexample"
-#endif
 
-#endif /* __iotivityexample_H__ */
+typedef struct appdata {
+	Evas_Object *win;
+	Evas_Object *layout;
+	Evas_Object *conform;
+	Evas_Object *nf;
+	location_manager_s *manager;
+	Ecore_Thread *thread;
+} appdata_s;
+
+#endif /* __basicui_H__ */
