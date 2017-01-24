90f86b6fcf5ea4749aea5a7cd308ced6ad7c1a2c
Philippe Coval <philippe.coval@osg.samsung.com>
2017-01-24 18:33:07 +0100

    WIP: mod: aircon_controller-auth.sh (sandbox/pcoval/cloud)
    Change-Id: Ied6aba79a033d0451da84152f34edbe071b35b8f
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- aircon_controller-auth.sh
+++ aircon_controller-auth.sh
@@ -1,2 +1,2 @@
 set -x
-./aircon_controller 172.17.0.1:5683 github 0d7bfea9a565390cd7a8 0   2>&1 | tee "$0.log"
+./aircon_controller 172.17.0.1:5683 github 147b0ca1bfacf80a109e 0   2>&1 | tee "$0.log"
