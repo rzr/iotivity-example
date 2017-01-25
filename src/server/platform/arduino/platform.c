ec95302269ee1778524db464ea2ecc60bbd25905
Philippe Coval <philippe.coval@osg.samsung.com>
2017-01-25 20:31:29 +0100

    WIP: mod: src/server/platform/arduino/platform.c (sandbox/pcoval/arduino-geolocation)
    Change-Id: I9e08718f5d3b70502cb7e78127a65e2ab3cc351e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/server/platform/arduino/platform.c
+++ src/server/platform/arduino/platform.c
@@ -76,6 +76,20 @@ unsigned int sleep(unsigned int secs)
 }
 
 
+#include <SFE_BMP180.h>
+int platform_getValue()
+{
+    double value=0;
+    char status=0;
+    static SFE_BMP180 sensor;
+    static int once=1;
+    if (once) { once--;
+        status = sensor.startTemperature();
+    }
+    status = sensor.getTemperature(value);
+    return (int) value;
+}
+
 void platform_setValue(bool value)
 {
     digitalWrite(gGpio, (value) ? HIGH : LOW);
