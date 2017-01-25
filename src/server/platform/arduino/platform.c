e09ffbf3cd450c58ec6ad69ec9dbfb530883d311
Philippe Coval <philippe.coval@osg.samsung.com>
2017-01-25 22:18:42 +0100

    WIP: mod: src/server/platform/arduino/platform.c (sandbox/pcoval/wip-watchdoc)
    Change-Id: I7139745fa5845ebabc221f2cf7e2bb94e236268e
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/server/platform/arduino/platform.c
+++ src/server/platform/arduino/platform.c
@@ -76,26 +76,13 @@ unsigned int sleep(unsigned int secs)
 }
 
 #if 0
-#include <SFE_BMP180.h>
 int platform_getValue()
 {
     double value=0;
-    char status=0;
-    static SFE_BMP180 sensor;
-    static int once=1;
-    if (once) { once--;
-        status = sensor.startTemperature();
-    }
-    status = sensor.getTemperature(value);
     return (int) value;
 }
 #else
-int platform_getValue()
-{
-    double value=0;
-    return (int) value;
-}
-
+#include "illuminance.c"
 #endif
 
 void platform_setValue(bool value)
