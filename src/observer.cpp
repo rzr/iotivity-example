406c735066c18e766082ed6fdc6d7a9969a83976
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 00:16:02 +0100

    WIP: mod: src/observer.cpp (sandbox/pcoval/geolocation)
    Change-Id: Ib3d6821abba0950d4cc1ee0bbb155efab1e68654
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- src/observer.cpp
+++ src/observer.cpp
@@ -174,19 +174,19 @@ void IoTObserver::print(shared_ptr<OCResource> resource)
 void IoTObserver::handle(const HeaderOptions headerOptions, const OCRepresentation &rep,
                          const int &eCode, const int &sequenceNumber)
 {
-    double lat = 0;
-    double lon = 0;
+    double latitude = 0;
+    double longitude = 0;
 
-    if (rep.hasAttribute("lat"))
+    if (rep.hasAttribute("latitude"))
     {
-        lat = rep.getValue<double>("lat");
+        latitude = rep.getValue<double>("latitude");
     }
-    if (rep.hasAttribute("lon"))
+    if (rep.hasAttribute("longitude"))
     {
-        lon = rep.getValue<double>("lon");
+        longitude = rep.getValue<double>("longitude");
     }
 
-    cout << "location: " << lat << ", " << lon<<endl;
+    cout << "location: " << latitude << ", " << longitude<<endl;
 }
 
 
