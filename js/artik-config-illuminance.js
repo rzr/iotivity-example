f9818668e3460e5d07f510e3615b90dc7ef2d585
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 15:11:04 +0100

    WIP: add: js/artik-config-illuminance.js (sandbox/pcoval/demo)
    Change-Id: I743bd49adbe0ae46ffba9a5c8ae50fc65425bdd3
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ js/artik-config-illuminance.js
@@ -0,0 +1,6 @@
+exports.Config = 
+    { deviceType : "oic.r.sensor.illuminance",
+      deviceId: "36f19870f7824264bdfce8cab95a8091",
+      deviceTypeId: "dt48751b456a2d458293ec129553e17e9b",
+      token: "9aef18269d3a499fa396c1ebe1f6cf11" 
+    };
