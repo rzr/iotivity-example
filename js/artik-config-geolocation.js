36b4354788c9958d522a77d691498377c421c749
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 15:11:04 +0100

    WIP: add: js/artik-config-geolocation.js (sandbox/pcoval/demo)
    Change-Id: Id50e29a95354952419beb253ae62d750d7dace0b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ js/artik-config-geolocation.js
@@ -0,0 +1,6 @@
+exports.Config = 
+    { deviceType : "org.example.r.sensor.geolocation.illumminance",
+      deviceId: "75818b48258548cbbdc51adbd222391d",
+      deviceTypeId: "dt007a9faa3b8f42d8baa3bd4a8689788b",
+      token: "e09e9a9980cb4f0fb4c1f255bc2d4f49"
+    };
