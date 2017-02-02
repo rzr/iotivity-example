c407158831d626215cef771f5e69670ccd009932
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 15:11:06 +0100

    WIP: add: js/switch-server-gpio.js (sandbox/pcoval/demo)
    Change-Id: I097d6fce80b95c015ba8f7967aa1c7f1d98ac690
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ js/switch-server-gpio.js
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+// Copyright 2016 Intel Corporation
+// Copyright 2016 Samsung Electronics France SAS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var sampleUri = "/BinarySwitchResURI";
+var sampleResourceType = "oic.r.switch.binary";
+
+var intervalId,
+handleReceptacle = {},
+
+// This is the same value as server.get.js
+sampleUri = sampleUri,
+iotivity = require( "iotivity-node/lowlevel" );
+
+console.log( "Starting OCF stack in server mode : "  + sampleUri);
+
+// Start iotivity and set up the processing loop
+iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
+
+iotivity.OCSetDeviceInfo( {
+    specVersion: "res.1.0.0",
+    dataModelVersions: [ "abc.0.0.1" ],
+    deviceName: "server.get",
+    types: []
+} );
+iotivity.OCSetPlatformInfo( {
+    platformID: "server.get.sample",
+    manufacturerName: "iotivity-node"
+} );
+
+intervalId = setInterval( function() {
+    iotivity.OCProcess();
+}, 1000 );
+
+console.log( "Registering resource" );
+var gpio = require("pi-gpio");
+var pin = 29;
+
+function setValue(value)
+{
+    console.log( (1 == value) ? "value: 1" : "value: 0");
+    gpio.open(pin,
+              "output", 
+              function(err) {
+                  gpio.write(pin, value, 
+                             function() {
+                                 gpio.close(pin);
+                             });
+              }
+              );
+}
+
+// Create a new resource
+iotivity.OCCreateResource(
+
+    // The bindings fill in this object
+    handleReceptacle,
+
+    sampleResourceType,
+    iotivity.OC_RSRVD_INTERFACE_DEFAULT,
+    sampleUri,
+    function( flag, request ) {
+	console.log( "Entity handler called with flag = " + flag + " and the following request:" );
+	console.log( JSON.stringify( request, null, 4 ) );
+
+	if ( request && request.payload && request.payload.values) {
+            setValue( request.payload.values.value );
+	    iotivity.OCDoResponse( {
+		requestHandle: request.requestHandle,
+		resourceHandle: request.resource,
+		ehResult: iotivity.OCEntityHandlerResult.OC_EH_OK,
+		payload: {
+		    type: iotivity.OCPayloadType.PAYLOAD_TYPE_REPRESENTATION,
+		    values: {
+                        "value" : request.payload.values.value
+		    }
+		},
+		resourceUri: sampleUri,
+		sendVendorSpecificHeaderOptions: []
+	    } );
+
+	    return iotivity.OCEntityHandlerResult.OC_EH_OK;
+	}
+
+	// By default we error out
+	return iotivity.OCEntityHandlerResult.OC_EH_ERROR;
+    },
+    iotivity.OCResourceProperty.OC_DISCOVERABLE );
+
+console.log( "Server ready" );
+
+// Exit gracefully when interrupted
+process.on( "SIGINT", function() {
+    console.log( "SIGINT: Quitting..." );
+
+    // Tear down the processing loop and stop iotivity
+    clearInterval( intervalId );
+    iotivity.OCDeleteResource( handleReceptacle.handle );
+    iotivity.OCStop();
+
+    // Exit
+    process.exit( 0 );
+} );
