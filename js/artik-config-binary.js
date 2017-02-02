7003af16e084bd1f19307e2d2d96dbe12cdc286c
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 15:11:04 +0100

    WIP: add: js/artik-config-binary.js (sandbox/pcoval/demo)
    Change-Id: Id28f7e5d62d58a7e8225a6090d94048eb339ac0b
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ js/artik-config-binary.js
@@ -0,0 +1,6 @@
+exports.Config = 
+    { deviceType : "org.example.binary",
+      deviceId: "6b71b5fb2cf845f192ae72cf4298fcd7",
+      deviceTypeId: "dt14f2e12d241a4b8f945e01b2f0db4473",
+      token: "126fd4431cdc4b069b1dbf3d99e0f422"
+    };
