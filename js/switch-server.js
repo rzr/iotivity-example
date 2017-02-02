0d3c2a4ca23889959a77b43f1f782d74ac5807b4
Philippe Coval <philippe.coval@osg.samsung.com>
2017-02-02 15:11:06 +0100

    WIP: mod: js/switch-server.js (sandbox/pcoval/demo)
    Change-Id: I5e0344f8c10e8318106e4704925b8a212462ba58
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- js/switch-server.js
+++ js/switch-server.js
@@ -14,87 +14,164 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var sampleUri = "/BinarySwitchResURI";
-var sampleResourceType = "oic.r.switch.binary";
 
-var intervalId,
-handleReceptacle = {},
+var delay = 1000;
 
-// This is the same value as server.get.js
-sampleUri = sampleUri,
-iotivity = require( "iotivity-node/lowlevel" );
+var gValues = { value: false };
+var EventEmitter = require('events').EventEmitter;
 
-console.log( "Starting OCF stack in server mode : "  + sampleUri);
+function Source()
+{
+    var self = this;
+    self.start = function() {
+   
+        var interval = setInterval(function(){
+            gValues.value != gValues.value;
+            emit
+        }, delay);
+    }
+}
 
-// Start iotivity and set up the processing loop
-iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
+exports.main = function()
+{
+    var sampleUri = "/BinaryResURI";
+    var sampleResourceType = "oic.switch.binary";
 
-iotivity.OCSetDeviceInfo( {
-    specVersion: "res.1.0.0",
-    dataModelVersions: [ "abc.0.0.1" ],
-    deviceName: "server.get",
-    types: []
-} );
-iotivity.OCSetPlatformInfo( {
-    platformID: "server.get.sample",
-    manufacturerName: "iotivity-node"
-} );
+    var intervalId,
+    handleReceptacle = {},
 
-intervalId = setInterval( function() {
-    iotivity.OCProcess();
-}, 1000 );
+    // This is the same value as server.get.js
+    sampleUri = sampleUri,
+    iotivity = require( "iotivity-node/lowlevel" );
 
-console.log( "Registering resource" );
+    console.log( "Starting OCF stack in server mode : "  + sampleUri);
 
-// Create a new resource
-iotivity.OCCreateResource(
+    // Start iotivity and set up the processing loop
+    iotivity.OCInit( null, 0, iotivity.OCMode.OC_SERVER );
 
-    // The bindings fill in this object
-    handleReceptacle,
+    iotivity.OCSetDeviceInfo( {
+        specVersion: "res.1.0.0",
+        dataModelVersions: [ "abc.0.0.1" ],
+        deviceName: "server.get",
+        types: []
+    } );
+    iotivity.OCSetPlatformInfo( {
+        platformID: "server.get.sample",
+        manufacturerName: "iotivity-node"
+    } );
 
-    sampleResourceType,
-    iotivity.OC_RSRVD_INTERFACE_DEFAULT,
-    sampleUri,
-    function( flag, request ) {
-	console.log( "Entity handler called with flag = " + flag + " and the following request:" );
-	console.log( JSON.stringify( request, null, 4 ) );
 
-	if ( request && request.payload && request.payload.values) {
+    console.log( "Registering resource" );
 
-	    console.log( request.payload.values.value ? "ON" : "OFF")
-	    iotivity.OCDoResponse( {
-		requestHandle: request.requestHandle,
-		resourceHandle: request.resource,
-		ehResult: iotivity.OCEntityHandlerResult.OC_EH_OK,
-		payload: {
+    var handleReceptacle = {}, observerIds = [];
+
+    function notify(values)
+    {
+        console.log( "notify: observers=" 
+                     +( observerIds.length > 0 ? 
+                        + observerIds.length 
+                        : 0));
+
+        if ( observerIds.length > 0 ) {
+	    iotivity.OCNotifyListOfObservers(
+	        handleReceptacle.handle,
+	        observerIds,
+	        {
 		    type: iotivity.OCPayloadType.PAYLOAD_TYPE_REPRESENTATION,
-		    values: {
-                        "value" : request.payload.values.value
+		    values: values
+	        },
+	        iotivity.OCQualityOfService.OC_HIGH_QOS 
+            );
+        }
+    }
+
+    // Create a new resource
+    iotivity.OCCreateResource(
+
+        // The bindings fill in this object
+        handleReceptacle,
+
+        sampleResourceType,
+        iotivity.OC_RSRVD_INTERFACE_DEFAULT,
+        sampleUri,
+        function( flag, request ) {
+	    console.log( "Entity handler called with flag = " + flag + " and the following request:" );
+	    console.log( JSON.stringify( request, null, 4 ) );
+
+	    if ( flag & iotivity.OCEntityHandlerFlag.OC_OBSERVE_FLAG ) {
+	        if ( request.obsInfo.obsId !== 0 ) {
+		    if ( request.obsInfo.action === iotivity.OCObserveAction.OC_OBSERVE_REGISTER ) {
+                        
+		        // Add new observer to list.
+		        observerIds.push( request.obsInfo.obsId );
+		    } else if ( request.obsInfo.action ===
+			        iotivity.OCObserveAction.OC_OBSERVE_DEREGISTER ) {
+                        
+		        // Remove requested observer from list.
+		        observerIdIndex = observerIds.indexOf( request.obsInfo.obsId );
+		        if ( observerIdIndex >= 0 ) {
+			    observerIds.splice( observerIdIndex, 1 );
+		        }
 		    }
-		},
-		resourceUri: sampleUri,
-		sendVendorSpecificHeaderOptions: []
-	    } );
+	        }
+	    }
 
-	    return iotivity.OCEntityHandlerResult.OC_EH_OK;
-	}
+	    if ( request) {
+	        iotivity.OCDoResponse( {
+		    requestHandle: request.requestHandle,
+		    resourceHandle: request.resource,
+		    ehResult: iotivity.OCEntityHandlerResult.OC_EH_OK,
+		    payload: {
+		        type: iotivity.OCPayloadType.PAYLOAD_TYPE_REPRESENTATION,
+		        values: gValues
+		        
+		    },
+		    resourceUri: sampleUri,
+		    sendVendorSpecificHeaderOptions: []
+	        } );
 
-	// By default we error out
-	return iotivity.OCEntityHandlerResult.OC_EH_ERROR;
-    },
-    iotivity.OCResourceProperty.OC_DISCOVERABLE );
+	        return iotivity.OCEntityHandlerResult.OC_EH_OK;
+	    }
 
-console.log( "Server ready" );
+	    // By default we error out
+	    return iotivity.OCEntityHandlerResult.OC_EH_ERROR;
+        },
+        iotivity.OCResourceProperty.OC_DISCOVERABLE |
+            iotivity.OCResourceProperty.OC_OBSERVABLE
+    );
 
-// Exit gracefully when interrupted
-process.on( "SIGINT", function() {
-    console.log( "SIGINT: Quitting..." );
+    console.log( "Server ready" );
 
-    // Tear down the processing loop and stop iotivity
-    clearInterval( intervalId );
-    iotivity.OCDeleteResource( handleReceptacle.handle );
-    iotivity.OCStop();
+    // Exit gracefully when interrupted
+    process.on( "SIGINT", function() {
+        console.log( "SIGINT: Quitting..." );
 
-    // Exit
-    process.exit( 0 );
-} );
+        // Tear down the processing loop and stop iotivity
+        clearInterval( intervalId );
+        iotivity.OCDeleteResource( handleReceptacle.handle );
+        iotivity.OCStop();
+
+        // Exit
+        process.exit( 0 );
+    } );
+
+    var source = Source();
+    
+    source.start();
+    source.on("update", function update(values){
+        console.log(values);
+        if ( gValues != values) {
+            gValues = values;
+            notify(gValues);
+        }
+    });
+
+    intervalId = setInterval( function() {
+        iotivity.OCProcess();
+    }, 1000 );
+}
+
+
+if (require.main === module) {
+    exports.main();
+}
