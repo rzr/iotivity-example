e72eddbfca03b3d5269b4933517d63306ebe1e4e
Philippe Coval <philippe.coval@osg.samsung.com>
2017-03-07 16:29:39 +0100

    tizen: Build for latest version
    Change-Id: I3fce873b8885ddbd29c4d4ebb1fd07f37330500a
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- extra/setup.sh
+++ extra/setup.sh
@@ -183,7 +183,7 @@ build_()
 #           url="http://github.com/iotivity/${package}"
 #           branch="master"
             url="http://github.com/tizenteam/${package}"
-            branch="sandbox/pcoval/on/previous/tizen"
+            branch="sandbox/pcoval/on/latest/tizen"
             args=" -b "${branch}""
             args=" $args --depth 1"
             ls "$package" || \
