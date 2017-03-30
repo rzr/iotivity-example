51edd4591d77aa0bac1233753c1c8d40e84dada5
Philippe Coval <philippe.coval@osg.samsung.com>
2017-03-30 15:57:10 +0200

    example: Import source to show discovery
    It was tested using IoTivity-1.2.0
    
    For the record, this change is squashing earlier versions of iotivity-example
    into a minimal stub (can be used for switch, mraa, dummy, geolocation etc)
    
    Change-Id: ab002b0fedd00b8d9a3d60e0aa949a03e3860fa5
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

---
+++ src/client.h
@@ -0,0 +1,70 @@
+// -*-c++-*-
+//******************************************************************
+//
+// Copyright 2014 Intel Corporation.
+// Copyright 2015 Eurogiciel <philippe.coval@eurogiciel.fr>
+// Copyright 2016 Samsung <philippe.coval@osg.samsung.com>
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+#include <memory>
+#include <iostream>
+
+#include <iotivity/resource/OCApi.h>
+#include <iotivity/resource/OCPlatform.h>
+#include <iotivity/resource/OCResource.h>
+
+
+class Resource
+{
+    public:
+        Resource(std::shared_ptr<OC::OCResource> resource);
+        virtual ~Resource();
+    protected:
+        std::shared_ptr<OC::OCResource> m_OCResource;
+        OC::OCRepresentation m_Representation;
+};
+
+
+class IoTClient
+{
+    public:
+        static int main(int argc, char *argv[]);
+
+        static void input();
+
+    public:
+        std::shared_ptr<Resource> getResource();
+        void start();
+        void print(std::shared_ptr<OC::OCResource> resource);
+        static IoTClient *getInstance();
+    private:
+        IoTClient();
+        virtual ~IoTClient();
+        void init();
+        void onFind(std::shared_ptr<OC::OCResource>);
+    private:
+        static IoTClient *mInstance;
+        std::shared_ptr<Resource> m_Resource;
+        std::shared_ptr<OC::PlatformConfig> m_platformConfig;
+        OC::FindCallback m_FindCallback;
+};
+
+#endif /* CLIENT_H_ */
