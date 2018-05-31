03b60750e8b0db584d951a10a143be6c9ffd1981
Philippe Coval <p.coval@samsung.com>
2018-05-31 22:26:07 +0200

    WIP: mod: rules/iotivity.mk (sandbox/pcoval/arduino)
    Change-Id: Idcfdea062bdd7f93b9f9ca16f6858fc7d6fa5d4b
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- rules/iotivity.mk
+++ rules/iotivity.mk
@@ -24,7 +24,7 @@ export git
 
 # Override here if needed
 iotivity_rel?=1.2-rel
-#iotivity_version=${iotivity_rel}
+iotivity_version=${iotivity_rel}
 #iotivity_src?=${HOME}/usr/local/src/
 
 # Supported configurations
